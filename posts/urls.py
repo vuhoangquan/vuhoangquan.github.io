61fd566b4c881b8149ea22706b97288be3a77985
vu hoang quan <43520713+vuhoangquan@users.noreply.github.com>
2021-01-18 09:20:44 +0700

    created aboutus page

--- posts/urls.py
+++ posts/urls.py
@@ -9,5 +9,7 @@ urlpatterns = [
     path("", views.index, name="index"),
     path("<int:post_id>", views.post, name="post"),
     path("author/<int:author_id>", views.author, name="author"),
+    path("aboutus/", views.aboutus, name="aboutus"),
+    # path("search/", views.search, name="search")
     # path("static/post.<int:post_id>/thumbnail.url", views.thumbnail, name="thumbnail"),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- posts/views.py
+++ posts/views.py
@@ -1,21 +1,51 @@
 from django.shortcuts import render
 from django.http import JsonResponse
+from django.template.loader import render_to_string
 from django.urls import reverse
+
 from .models import Post, Author, Category, SubCategory
 
 from itertools import chain
 
 # Create your views here.
 def index(request):
+    url_parameter = request.GET.get("title")
+    if url_parameter:
+        searches = Post.objects.filter(title__icontains=url_parameter)
+    
+        if request.is_ajax():
+            html = render_to_string(
+                template_name="posts/partial_result.html", 
+                context={"posts_returned": searches}
+            )
+            data_dict = {"html_from_view": html}
+            return JsonResponse(data=data_dict, safe=False)
+    else:
+        searches = Post.objects.all()
+        
     return render(request, "posts/index.html", {
         "posts":Post.objects.all(),
         "categories":Category.objects.all(),
-        "sub_categories":SubCategory.objects.all()
+        "sub_categories":SubCategory.objects.all(),
     })
-
+    
 def post(request, post_id):
     post = Post.objects.get(id=post_id)
     categories = Category.objects.filter(posts = post).all()
+    
+    url_parameter = request.GET.get("title")
+    if url_parameter:
+        searches = Post.objects.filter(title__icontains=url_parameter)
+    
+        if request.is_ajax():
+            html = render_to_string(
+                template_name="posts/partial_result.html", 
+                context={"posts_returned": searches}
+            )
+            data_dict = {"html_from_view": html}
+            return JsonResponse(data=data_dict, safe=False)
+    else:
+        searches = Post.objects.all()
     
     return render(request, "posts/post.html", {
         "post":post,
@@ -50,7 +80,38 @@ def author(request, author_id):
         "sub_categories":SubCategory.objects.all()
     })
     
+def aboutus(request):
+    tag_cate = Category.objects.all()
+    tag_subcate = SubCategory.objects.all()
     
+    return render(request, "posts/aboutus.html",{
+        "categories": tag_cate,
+        "subcategories": tag_subcate,
+    })
+    
+# def search(request):
+#     ctx={}
+    # search?post=<sth_post>&author=<sth_author>
+    # post_url   = request.GET.get("post")
+    # author_url = request.GET.get("author")
+    # posts_returned = Post.object.filter(title__icontains=post_url)
+    # author_returned= Author.objects.filter(name__icontains=author_url)
+    # ctx["posts"] = post_returned
+    # ctx["authors"] = author_returned
+
+    # if request.is_ajax():
+    #     html = render_to_string(
+    #         template_name="partial_result.html", 
+    #         context={"posts_returned": posts_returned,"author_returned":author_returned}
+    #     )
+    #     data_dict = {"html_from_view": html}
+    #     return JsonResponse(data=data_dict, safe=False)
+
+    # return render(request, "search.html", context=ctx)
+    # return JsonResponse({
+    #     "post_search": posts_returned,
+    #     "author_search": author_returned,
+    # })
     
     
 # def thumbnail(request, post_id):
